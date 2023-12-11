bbfc2de0fb434a9a274c0bfbc54ddcbf5de074f4
Amrutha <Amrutha@vozi.in>
2023-12-11 12:02:28 +0530

    11th dec commit

--- Demo/static/dashboard.js
+++ Demo/static/dashboard.js
@@ -1,31 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll("#sidebar nav ul a");
+  const links = document.querySelectorAll("#sidebar nav ul a");
 
-    links.forEach(link => {
-      link.addEventListener("click", function (event) {
-        event.preventDefault();
-        const target = this.getAttribute("href").substring(1);
-        window.location.href = `/${target.toLowerCase().replace(" ", "-")}`;
-      });
+  links.forEach(link => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const target = this.getAttribute("href").substring(1);
+      window.location.href = `/${target.toLowerCase().replace(" ", "-")}`;
     });
   });
-
-  document.querySelectorAll('.know-more').forEach(button => {
-    button.addEventListener('click', function() {
-        alert('More details to come!');
-    });
 });
 
-document.querySelectorAll('.know-more-one').forEach(button => {
+document.querySelectorAll('.know-more').forEach(button => {
   button.addEventListener('click', function() {
       alert('More details to come!');
   });
 });
 
+document.querySelectorAll('.know-more-one').forEach(button => {
+button.addEventListener('click', function() {
+    alert('More details to come!');
+});
+});
+
 
 document.addEventListener('DOMContentLoaded', function() {
-  var knowMoreButton = document.querySelector('.know-more-two');
-  knowMoreButton.addEventListener('click', function() {
-      alert('More information about driver performance.');
-  });
+var knowMoreButton = document.querySelector('.know-more-two');
+knowMoreButton.addEventListener('click', function() {
+    alert('More information about driver performance.');
+});
 });
